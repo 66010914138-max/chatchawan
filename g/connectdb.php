db5cce486aa2257ef41cbf890bf1a65f3f759a2b
chatchawan <goffari2546@gmail.com>
2026-01-28 16:12:14 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -2,7 +2,7 @@
 		$host = "localhost";
 		$user = "root";
 		$pwd = "Golf@2004";
-		$db = "4138db";
-		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+		$db = "4122db";
+		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่SASSอมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
 ?>
