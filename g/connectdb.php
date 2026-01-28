25163bee27f6e1543d5222c047ab6fe7f6ed3a1a
chatchawan <goffari2546@gmail.com>
2026-01-28 16:13:17 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -2,7 +2,7 @@
 		$host = "localhost";
 		$user = "root";
 		$pwd = "Golf@2004";
-		$db = "4122db";
+		$db = "4138db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่SASSอมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
 ?>
