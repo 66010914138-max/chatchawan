de83ad54c19d22b8ee04e85695b33e2b2bb4ef97
chatchawan <66010914138@msu.ac.th>
2026-02-04 16:24:37 +0700

    ฟ

--- h/connectdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-		$host = "localhost";
-		$user = "root";
-		$pwd = "Golf@2004";
-		$db = "4138db";
-		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่SASSอมต่อฐานข้อมูลไม่ได้");
-		mysqli_query($conn, "SET NAMES utf8");
-?>
