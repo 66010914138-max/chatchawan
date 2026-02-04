1f4efa5838dcd5bca768e6b66c0754b66bb97c7a
chatchawan <66010914138@msu.ac.th>
2026-02-04 17:31:43 +0700

    ฟ

---
+++ aaa/a.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชัชวาล สิงห์เทศ (แบงค์)</title>
+</head>
+
+<body>
+<h1>a.php</h1>
+<?php
+	$_SESSION['name']="ชัชวาล สิงห์เทศ (แบงค์)";
+	$_SESSION['nickname'] = "แบงค์";
+	$_SESSION['p1']= "พวงกุญแจ";
+	$_SESSION['p2'] = "การ์ด";
+
+?>
+</body>
+</html>
