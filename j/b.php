7e846c6c5fa32b7f2660783fadd13ed9b0d5f553
66010914138-max <66010914138@msu.ac.th>
2026-02-18 13:33:07 +0700

    Add files via upload

---
+++ j/b.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ชัชวาล สิงห์เทศ(แบงค์)</title>
+</head>
+
+<body>
+<h1>ชัชวาล สิงห์เทศ(แบงค์)</h1>
+
+<img src="img/1.jpg" width="100" >
+</body>
+</html>
