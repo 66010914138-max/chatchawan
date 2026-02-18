65e3500bc73f64ebd5ac54d51fe39a2ec63b830c
66010914138-max <66010914138@msu.ac.th>
2026-02-18 10:08:55 +0700

    Update b.php

--- i/b.php
+++ i/b.php
@@ -2,19 +2,21 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>งาน i -- ชัชวาล สิงห์เทศ 66010914138</title>
+    <title>จัดการจังหวัด -- ชัชวาล สิงห์เทศ 66010914138</title>
 </head>
 <body>
-<h1>งาน i -- ชัชวาล สิงห์เทศ 66010914138</h1>
+<h1>งาน i -- จัดการจังหวัด (b.php)</h1>
 
 <form method="post" action="" enctype="multipart/form-data">
     ชื่อจังหวัด: <input type="text" name="pname" autofocus required><br>
     รูปภาพ: <input type="file" name="pimage" required><br>
     ภาค: 
-    <select name="rid">
+    <select name="rid" required>
+        <option value="">-- เลือกภาค --</option>
         <?php 
         include_once("connectdb.php");
-        $sql_reg = "SELECT * FROM `regions`";
+        // เชื่อมโยงข้อมูลภาคจากหน้า a.php
+        $sql_reg = "SELECT * FROM `regions` ORDER BY r_name ASC";
         $rs_reg = mysqli_query($conn, $sql_reg);
         while ($data_reg = mysqli_fetch_array($rs_reg)){
             echo "<option value='".$data_reg['r_id']."'>".$data_reg['r_name']."</option>";
@@ -28,7 +30,7 @@
 
 <?php
 if(isset($_POST['Submit'])){
-    $pname = $_POST['pname'];
+    $pname = mysqli_real_escape_string($conn, $_POST['pname']);
     $rid = $_POST['rid'];
     $ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
     
@@ -39,41 +41,46 @@ if(isset($_POST['Submit'])){
         if(!file_exists("img")) { mkdir("img"); }
         move_uploaded_file($_FILES['pimage']['tmp_name'], "img/".$last_id.".".$ext);
         
-        // จุดที่ 1: แก้จาก a.php เป็น b.php เพื่อให้บันทึกแล้วอยู่ที่เดิม
         echo "<script>alert('บันทึกสำเร็จ'); window.location='b.php';</script>";
     }
 }
 ?>
 
 <h3>รายการจังหวัด</h3>
-<table border="1" width="600">
+<table border="1" width="700">
     <tr bgcolor="#cccccc">
         <th>รหัสจังหวัด</th>
         <th>ชื่อจังหวัด</th>
-        <th>รูปจังหวัด</th>
+        <th>ชื่อภาค</th> <th>รูปจังหวัด</th>
         <th>ลบ</th>
     </tr>
 
 <?php
-$sql_show = "SELECT * FROM `provinces` ORDER BY p_id DESC";
+// เชื่อมตาราง provinces กับ regions เพื่อเอาชื่อภาค (r_name) มาแสดง
+$sql_show = "SELECT provinces.*, regions.r_name 
+             FROM `provinces` 
+             LEFT JOIN `regions` ON provinces.r_id = regions.r_id 
+             ORDER BY provinces.p_id DESC";
+             
 $rs_show = mysqli_query($conn, $sql_show);
 while ($data = mysqli_fetch_array($rs_show)){
 ?>
     <tr>
         <td align="center"><?php echo $data['p_id']; ?></td>
         <td><?php echo $data['p_name']; ?></td>
-        <td align="center">
-            <img src="img/<?php echo $data['p_id'].".".$data['p_ext']; ?>" width="100" height="100" onerror="this.src='img/no-image.png'">
+        <td align="center"><?php echo $data['r_name']; ?></td> <td align="center">
+            <img src="img/<?php echo $data['p_id'].".".$data['p_ext']; ?>" width="100" height="100" style="object-fit:cover" onerror="this.src='img/no-image.png'">
         </td>
         <td align="center">
             <a href="remove_province.php?id=<?php echo $data['p_id']; ?>" onclick="return confirm('ยืนยันการลบจังหวัดนี้?');">
-                <img src="img/delete.jpg" width="50" alt="ลบ">
+                <img src="img/delete.jpg" width="40" alt="ลบ">
             </a>
         </td>
     </tr>
 <?php } ?>
 </table>
-
+<br>
+<a href="a.php"><- กลับไปหน้าจัดการภาค</a>
 </body>
 </html>
 <?php mysqli_close($conn); ?>
