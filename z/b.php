e9d5b231a221edb121c33d4a9a96c24d52eb5f78
66010914138-max <66010914138@msu.ac.th>
2026-02-25 00:49:15 +0700

    Update b.php

--- z/b.php
+++ z/b.php
@@ -1,14 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root"; // ปกติจะเป็น root
-$password = "Golf@2004";     // ปกติจะว่างไว้ถ้าใช้ XAMPP
-$dbname = "4138db"; // ชื่อฐานข้อมูลที่คุณสร้างไว้
-
-// สร้างการเชื่อมต่อ
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// เช็คการเชื่อมต่อ
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+		$host = "localhost";
+		$user = "root";
+		$pwd = "Golf@2004";
+		$db = "4138db";
+		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+		mysqli_query($conn, "SET NAMES utf8");
 ?>
